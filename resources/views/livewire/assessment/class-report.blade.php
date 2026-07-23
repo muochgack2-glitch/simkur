5a59a77c150bf7ec47216ec1d713e0dd03a7c8b2
muochgack2-glitch <muochgack2@gmail.com>
2026-07-23 19:47:33 +0700

    remove duplicate tabs in class-report page

--- resources/views/livewire/assessment/class-report.blade.php
+++ resources/views/livewire/assessment/class-report.blade.php
@@ -82,25 +82,6 @@
                     </nav>
                 </div>
             </div>
-                                </svg>
-                                Gaya Belajar (VARK)
-                            </span>
-                        </button>
-                        <button wire:click="switchTab('diagnostic')"
-                                class="w-1/2 border-b-2 py-4 px-1 text-center text-sm font-medium transition-colors
-                                {{ $activeTab === 'diagnostic' 
-                                    ? 'border-green-500 text-green-600' 
-                                    : 'border-transparent text-gray-700 hover:border-gray-300 hover:text-gray-700' }}">
-                            <span class="inline-flex items-center">
-                                <svg class="mr-2 h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
-                                </svg>
-                                Kesiapan Belajar (Diagnostik)
-                            </span>
-                        </button>
-                    </nav>
-                </div>
-            </div>
 
             @if($reportType === 'vark')
                 @include('livewire.assessment.partials.vark-class-report')
