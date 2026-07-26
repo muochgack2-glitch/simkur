bdeb35b2c9e4767b088b545dc36370a443a6a591
muochgack2-glitch <muochgack2@gmail.com>
2026-07-26 12:48:55 +0700

    Fix: Add \ method to Livewire internal methods handler

--- app/Providers/LivewireCommitFixServiceProvider.php
+++ app/Providers/LivewireCommitFixServiceProvider.php
@@ -18,18 +18,18 @@ class LivewireCommitFixServiceProvider extends ServiceProvider
     /**
      * Bootstrap services.
      * 
-     * This service provider fixes the Livewire $commit method issue.
-     * Livewire JavaScript sends $commit method calls, but PHP components
-     * don't have this method, causing MethodNotFoundException.
+     * This service provider fixes the Livewire internal method issues.
+     * Livewire JavaScript sends $commit and $set method calls, but PHP components
+     * don't have these methods, causing MethodNotFoundException.
      * 
-     * We use Livewire's 'call' hook to intercept $commit calls and return early.
+     * We use Livewire's 'call' hook to intercept these calls and return early.
      */
     public function boot(): void
     {
         Livewire::listen('call', function ($component, $method, $params, $context, $returnEarly) {
-            // If the method being called is $commit, return early with null
+            // If the method being called is $commit or $set, return early with null
             // This prevents the MethodNotFoundException from being thrown
-            if ($method === '$commit') {
+            if ($method === '$commit' || $method === '$set') {
                 $returnEarly(null);
             }
         });
