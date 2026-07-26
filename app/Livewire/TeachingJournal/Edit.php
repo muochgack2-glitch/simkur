01f92bccf5c8f070e76ff411ceab43312f5f8334
muochgack2-glitch <muochgack2@gmail.com>
2026-07-26 20:50:22 +0700

    fix: Use file_get_contents for Intervention Image v4 read method

--- app/Livewire/TeachingJournal/Edit.php
+++ app/Livewire/TeachingJournal/Edit.php
@@ -256,7 +256,7 @@ class Edit extends BaseComponent
         
         // Read and process image with Intervention Image v4
         $manager = new ImageManager(new Driver());
-        $image = $manager->read($photo->getRealPath());
+        $image = $manager->read(file_get_contents($photo->getRealPath()));
         
         // Resize to max 1024x1024 while maintaining aspect ratio
         $image->scale(width: 1024, height: 1024);
