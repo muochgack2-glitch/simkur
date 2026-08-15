e1f3de3866e6f8b59a00a0622948d1599fbf6505
muochgack2-glitch <muochgack2@gmail.com>
2026-08-15 21:15:40 +0700

    fix: pindah route PKL Field ke group admin/waka (bukan siswa)

--- routes/web.php
+++ routes/web.php
@@ -418,6 +418,11 @@ Route::prefix('api')->group(function () {
     Route::middleware('check.role:admin,kepala_sekolah,waka_kurikulum')->group(function () {
         Route::get('/pkl-learning-periods', \App\Livewire\PklLearning\PeriodManager::class)->name('pkl-learning.periods');
         Route::get('/pkl-learning-monitoring', \App\Livewire\PklLearning\Monitoring::class)->name('pkl-learning.monitoring');
+
+        // PKL Lapangan (DU/DI)
+        Route::get('/pkl-field/companies', \App\Livewire\PklField\CompanyManager::class)->name('pkl-field.companies');
+        Route::get('/pkl-field/placements', \App\Livewire\PklField\PlacementManager::class)->name('pkl-field.placements');
+        Route::get('/pkl-field/supervisors', \App\Livewire\PklField\SupervisorAssignment::class)->name('pkl-field.supervisors');
     });
 
     // Siswa: Akses Pembelajaran PKL
@@ -426,9 +431,5 @@ Route::prefix('api')->group(function () {
         Route::get('/pkl-learning/student/course/{course}', \App\Livewire\PklLearning\StudentCourse::class)->name('pkl-learning.student.course');
         Route::get('/pkl-learning/student/submission/{assignment}', \App\Livewire\PklLearning\StudentSubmission::class)->name('pkl-learning.student.submission');
         Route::get('/pkl-learning/student/quiz/{quiz}', \App\Livewire\PklLearning\StudentQuiz::class)->name('pkl-learning.student.quiz');
-        // PKL Lapangan (DU/DI)
-        Route::get('/pkl-field/companies', \App\Livewire\PklField\CompanyManager::class)->name('pkl-field.companies');
-        Route::get('/pkl-field/placements', \App\Livewire\PklField\PlacementManager::class)->name('pkl-field.placements');
-        Route::get('/pkl-field/supervisors', \App\Livewire\PklField\SupervisorAssignment::class)->name('pkl-field.supervisors');
     });
 
