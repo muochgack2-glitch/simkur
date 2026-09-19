a51b7ad5f09452c8c99181d07ae50be4e6a374ae
muochgack2-glitch <muochgack2@gmail.com>
2026-09-19 15:26:08 +0700

    chore: Remove accidentally committed scratch files, update .gitignore

--- scratch_write_blade.php
+++
@@ -1,4 +0,0 @@
-<?php
-file_put_contents(
-"C:/Users/DMCenter/Music/SPMB2/E-KALDIK/resources/views/livewire/assessment-label/index.blade.php",
-<<<'BLADE'
--- storage/scripts/copy_blade.py
+++
@@ -1,4 +0,0 @@
-import shutil
-src = r'C:\Users\DMCenter\Music\SPMB2\E-KALDIK\storage\scripts\blade_content.txt'
-dst = r'C:\Users\DMCenter\Music\SPMB2\E-KALDIK\resources\views\livewire\student-assessment\index.blade.php'
-shutil.copy2(src, dst)
